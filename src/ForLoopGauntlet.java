5d8138670d54239416d81ba24617e39f77a82912
league <league@wts20.attlocal.net>
2018-06-12 18:30:06 -0700

    ForLoopGauntlet

--- src/ForLoopGauntlet.java
+++ src/ForLoopGauntlet.java
@@ -2,24 +2,65 @@
 public class ForLoopGauntlet {
 public static void main(String[] args) {
 	
-for (int i = 0; i <= 50; i++) {
-	if (i % 2 == 0) {
-		
-	}
-}
+// for (int i = 0; i <= 100; i++) {
+	// if (i % 2 == 1) {
+		// System.out.println(i);
+	// }
+// }
 	
+// for (int i = 0; i <= 500; i++) {
+	// if (i % 2 == 0) {
+		// System.out.println(i + " Even");
+	// }
+	// if (i % 2 == 1) {
+		// System.out.println(i + " Odd");
+	// }
+// }
 
+	// for (int i = 0; i <= 777; i++) {
+		// if (i % 7 == 0) {
+			// System.out.println(i);
+		// }
+	// }
 	
+	// for (int i = 0; i <= 12; i++) {
+		// System.out.print(2005 + i);
+		// System.out.println(" " + i + " years old.");
+	// }
 	
+	// for (int i = 0; i < 3; i++) {
+
+		// for (int x = 0; x <= 2; x++) {
+			// System.out.println(i + " " + x);
+		// }
+	// }
 	
+	 // for (int i = 1; i < 8; i+=3) {
 	
+		// for (int ins = i; ins < i+3; ins++) {
+			// System.out.print(ins);
+
+		// }	
+		// System.out.println();	
+	//}
 	
+/*	  for (int i = 1; i < 99; i+=10) {
+			
+		 for (int ins = i; ins < i+10; ins++) {
+			 System.out.print(ins + " ");
+
+		}	
+		System.out.println();	
+	}*/
 	
-	
-	
-	
-	
-	
+	 for (int i = 1; i < 6; i++) {
+			
+		for (int ins = 1; ins < i+1; ins++) {
+			System.out.print("* ");
+
+		}	
+		System.out.println();
+	}
 	
 	
 	
